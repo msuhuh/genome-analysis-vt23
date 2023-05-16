960225243cffb5e48c43e6adb869cf0b5e5c6709
Minna Sayehban <samuraii@rackham4.uppmax.uu.se>
2023-05-16 14:40:37 +0200

    Wrote batch script for using BLASTN to create synteny comparison.

---
+++ code/05_blastn_synteny.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 10:00:00
+#SBATCH -J misa1224_blastn_synteny
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user minna.sayehban.1224@student.uu.se
+
+# LOAD MODULES
+module load bioinfo-tools
+module load blast
+
+# SET FILES
+canu_in_file="/home/samuraii/genome-analysis-vt23/analyses/01_genome_assembly/assembly_canu.contigs.fasta"
+ref_in_file="/home/samuraii/genome-analysis-vt23/data/ml04_ref.fasta"
+out_folder="/home/samuraii/genome-analysis-vt23/analyses/05_synteny_comparison"
+
+# RUN BLASTN
+blastn -query $canu_in_file -subject $ref_in_file -outfmt 6 -out $out_folder/blastn.txt
