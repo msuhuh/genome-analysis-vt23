ed5616bf069241df91a129675cf1b3ac3a1b6e43
Minna Sayehban <samuraii@rackham2.uppmax.uu.se>
2023-08-24 16:28:49 +0200

    new htseq code

--- code/07_htseq_count_reads.sh
+++ code/07_htseq_count_reads.sh
@@ -13,12 +13,12 @@ module load bioinfo-tools
 module load htseq
 
 # SET FILE
-in_folder="/proj/genomeanalysis2023/nobackup/work/samuraii/06_bwa_output/"
+in_folder="/home/samuraii/genome-analysis-vt23/analyses/06_mapping_reads/"
 annotation_file="/home/samuraii/genome-analysis-vt23/analyses/03_structural_annotation/annotation_prokka.gff"
 out_folder="/home/samuraii/genome-analysis-vt23/analyses/07_count_mapped_reads/"
 
-# ITERATE OVER THE SORTED BAM FILES
-for sorted_bam_file in ERR2036629 ERR2036630 ERR2036631 ERR2036632 ERR2036633; do
-  # PERFORM READ COUNTING
-  htseq-count -f bam -r pos -s reverse -t CDS -i ID $in_folder${sorted_bam_file}_bwa_sorted.bam $annotation_file > $out_folder${sorted_bam_file}.txt
+# ITERATE OVER THE SORTED BAM FILES AND PERFORM READ COUNTING
+for i in ERR2036629 ERR2036630 ERR2036631 ERR2036632 ERR2036633
+do
+htseq-count -f bam -r pos -s reverse -t CDS -i ID $in_folder${i}.sorted.bam $annotation_file > $out_folder${i}.txt
 done
